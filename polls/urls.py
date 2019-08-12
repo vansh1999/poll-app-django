6cea26669d1332d5661c4cf0e01341efd88b9c85
vansh1999 <vansh.bhardwaj1999@gmail.com>
2019-08-12 23:42:38 +0530

    better variables in view,index,detail

--- polls/urls.py
+++ polls/urls.py
@@ -5,9 +5,13 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('' , views.index , name = "index"),
-    path('details/<int:question_id>/', views.detail, name="detail"),
+    # polls/home/
+    path('home/' , views.index , name = "index"),
+    # polls/details/3
+    path('detail/<int:question_id>/', views.detail, name="detail"),
+    # polls/results/3
     path('<int:question_id>/results/', views.results , name="results"),
+    # polls/vote/3
     path('<int:question_id>/vote/', views.vote, name="vote"),
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -11,15 +11,15 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:10]
 
     context = {
-        'latest_question_list': latest_question_list,
+        'latest_question_list_key': latest_question_list,
     }
     return render(request , 'polls/index.html' , context)
 
 
 
 def detail(request , question_id):
-    question = get_object_or_404(Question , pk = question_id)
-    return render(request , 'polls/detail.html' , {'question' : question})
+    question_front_end = get_object_or_404(Question , pk = question_id)
+    return render(request , 'polls/detail.html' , {'question_key' : question_front_end})
 
 
 def results(request , question_id):
